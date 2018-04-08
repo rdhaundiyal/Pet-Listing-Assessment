a8cc6412aa0a7460f91ff9b70c2f543d1d6dc0f2
rdhaundiyal@hotmail.com <rdhaundiyal@hotmail.com>
2018-04-08 11:01:39 +0530

    Initial Commit

---
+++ src/Components/Providers/AGL.Components.Providers.Inteface/Exception/RestException.cs
@@ -0,0 +1,10 @@
+﻿namespace AGL.Components.Providers.Inteface.Exception
+{
+   public class RestException:System.Exception
+    {
+        public RestException(string url, string message):base( string.Format("Url: {0} , Message:{1} ",url,message))
+        {
+
+        }
+    }
+}
---
+++ src/Components/Providers/AGL.Components.Providers.Inteface/IProvider.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+
+
+namespace AGL.Components.Providers.Inteface
+{
+    public interface IProvider
+    {
+        T Get<T>(string resourcel) where T : class, new();
+
+        Task<T> GetAsync<T>(string resource) where T : class, new();
+
+
+    }
+}
