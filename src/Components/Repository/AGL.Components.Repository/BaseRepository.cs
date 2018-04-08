47cd21d1104e71e1c0228b00812982875761c2a2
rdhaundiyal@hotmail.com <rdhaundiyal@hotmail.com>
2018-04-08 11:02:22 +0530

    Initial Commit

---
+++ src/Components/Repository/AGL.Components.Repository/BaseRepository.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using AGL.Components.Providers.Inteface;
+namespace AGL.Components.Repository
+{
+    public abstract class BaseRepository<T> where T : class
+    {
+        private readonly IProvider _serviceProvider;
+
+        protected BaseRepository(IProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+        public virtual IList<T> GetAll(string resource)
+        {
+            return _serviceProvider.Get<List<T>>(resource);
+
+          
+        }
+        public virtual T Get(string resource)
+        {
+            return null;
+        }
+        public virtual Task<T> GetAsync(string resource)
+        {
+            return null;
+        }
+
+
+    }
+}
---
+++ src/Components/Repository/AGL.Components.Repository/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AGL.Components.Repository
+{
+    public interface IRepository<T> where T : class
+    {
+        T Get(string resource);
+        Task<T> GetAsync(string resource);
+        IList<T> GetAll(string resource);
+
+    }
+}
