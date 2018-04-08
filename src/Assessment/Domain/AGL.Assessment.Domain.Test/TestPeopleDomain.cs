278e75cb6e2e7e28a18ec1271912825830017221
rdhaundiyal@hotmail.com <rdhaundiyal@hotmail.com>
2018-04-08 21:05:53 +0530

    added test classes

---
+++ src/Assessment/Domain/AGL.Assessment.Domain.Test/TestPeopleDomain.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using AGL.Assessment.Domain.Repositories;
+using AGL.Assessment.Domain.Dto;
+using System.Collections.Generic;
+
+namespace AGL.Assessment.Domain.Test
+{
+    [TestClass]
+    public class TestPeopleDomain
+    {
+        List<Person> PersonTestCollection;
+        [TestInitialize]
+        public void Initialize()
+        {
+            PersonTestCollection = new List<Person>();
+//PersonTestCollection.Add(new Person() { Age=0,Gender="male",Name="",Pets=new List<Pet>{})
+        }
+        [TestMethod]
+        public void Test_OwnerByPetType_Cat()
+        {
+            Mock<IPeopleRepository> peopleRepositoryMock = new Mock<IPeopleRepository>();
+            peopleRepositoryMock.Setup(k => k.GetAll(It.IsAny<string>())).Returns(PersonTestCollection);
+
+            var peopleDomain = new PeopleDomain(peopleRepositoryMock.Object);
+
+        }
+        [TestCleanup]
+        public void CleanUp()
+        {
+
+        }
+    
+    }
+}
---
+++ src/Components/Providers/AGL.Components.Providers.RestProviders.Test/TestRestSharpProvider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AGL.Components.Providers.RestProviders.Test
+{
+    [TestClass]
+    public class TestRestSharpProvider
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+         
+        }
+    }
+}
