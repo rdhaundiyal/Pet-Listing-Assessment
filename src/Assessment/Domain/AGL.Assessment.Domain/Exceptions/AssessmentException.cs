6925c32874ffddc2fee99473c8384d2df290a02a
rdhaundiyal@hotmail.com <rdhaundiyal@hotmail.com>
2018-04-08 16:05:12 +0530

    Adding error views

---
+++ src/Assessment/Domain/AGL.Assessment.Domain/Exceptions/AssessmentException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AGL.Assessment.Domain.Helpers;
+
+namespace AGL.Assessment.Domain.Exceptions
+{
+   public class AssessmentException:Exception
+    {
+       public string ErrorId { get; set; }
+       public AssessmentException() : base(ConfigSettings.GeneralExceptionMessage)
+       {
+           ErrorId = "AssessmentError";
+       }
+
+    }
+}
---
+++ src/Assessment/Web/Mvc/AGL.Assessment.Web.Mvc/Controllers/ErrorController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AGL.Components.Helpers;
+
+namespace AGL.Assessment.Web.Mvc.Controllers
+{
+    public class ErrorController : Controller
+    {
+        //
+        // GET: /Error/
+        public ActionResult Index(string errorId="")
+        {
+            object message = string.Empty;
+            if(!string.IsNullOrEmpty(errorId))
+            message = ConfigHelper.GetValue<string>(errorId);
+            return View(message );
+        }
+	}
+}
